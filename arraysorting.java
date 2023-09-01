9490264ef01ad7bf5f95faaeaf94fbc45b2e1e00
kushsengar <113542203+kushsengar@users.noreply.github.com>
2023-09-02 01:45:19 +0530

    Add files via upload
    some complex array operations.

---
+++ arraysorting.java
@@ -0,0 +1,10 @@
+public class arraysorting {
+    public static void main(String[] args){
+        String arr[]= {"kush","khushi","anuj","lovely","lalit","anu","deepak","bhoomi","mamta","amit"};
+        java.util.Arrays.sort(arr);
+        for (String x: arr) {
+            System.out.println(x);
+        }
+    }
+
+}
